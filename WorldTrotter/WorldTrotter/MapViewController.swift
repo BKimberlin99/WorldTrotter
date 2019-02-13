6ae46814e298f513338d7841f27495049ff8b7ab
Brandon Kimberlin <bkimberlin@humantorch.local>
2019-02-13 13:26:21 -0500

    Added MapViewController.swift file, attached asset icons for convert and map tabs, finish chapter 5, page 103

---
+++ WorldTrotter/WorldTrotter/MapViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MapViewController.swift
+//  WorldTrotter
+//
+//  Created by Brandon Kimberlin on 2/13/19.
+//  Copyright © 2019 Brandon Kimberlin. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("MapViewController loaded its view.")
+    }
+}
